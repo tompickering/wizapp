c9ca30733f46d011d383e8f90d2e3eb2ad13059e
Tom Pickering <tom@alephspace.net>
2019-01-31 19:32:34 +0000

    Remove this->

--- src/collectable.cpp
+++ src/collectable.cpp
@@ -11,144 +11,144 @@
 using std::string;
 
 Collectable::Collectable(int x, int y, bool flying) : Entity(x, y) {
-    this->name = "Collectable";
-    this->flying = flying;
-    this->navigable_h = true;
-    this->collected = false;
+    name = "Collectable";
+    flying = flying;
+    navigable_h = true;
+    collected = false;
     string type = flying ? "2" : "1";
 
     /* Defaults */
-    this->ctype = flying ? FMushroom : Flower;
+    ctype = flying ? FMushroom : Flower;
     float duration = 0.35f;
 
     if (flying) {
         switch (level_ref->theme) {
             case Valley:
-                this->ctype = FMushroom;
+                ctype = FMushroom;
                 break;
             case Industrial:
-                this->ctype = FMushroom;
+                ctype = FMushroom;
                 break;
             case Sun:
-                this->ctype = Energy;
+                ctype = Energy;
                 break;
             case Snowman:
-                this->ctype = FShadow;
+                ctype = FShadow;
                 break;
             case Jazz:
-                this->ctype = Note2;
+                ctype = Note2;
                 break;
             case Stereo:
-                this->ctype = Note3;
+                ctype = Note3;
                 break;
             case Star:
-                this->ctype = FStar1;
+                ctype = FStar1;
                 break;
             case Alien:
-                this->ctype = FStar2;
+                ctype = FStar2;
                 break;
             case Fantasia1:
-                this->ctype = FDiamond;
+                ctype = FDiamond;
                 break;
             case Fantasia2:
-                this->ctype = FUnknown;
+                ctype = FUnknown;
                 break;
         }
     } else {
         switch (level_ref->theme) {
             case Valley:
-                this->ctype = Flower;
+                ctype = Flower;
                 duration = 0.2f;
                 break;
             case Industrial:
-                this->ctype = Mushroom;
+                ctype = Mushroom;
                 break;
             case Sun:
-                this->ctype = Atom;
+                ctype = Atom;
                 break;
             case Snowman:
-                this->ctype = Shadow;
+                ctype = Shadow;
                 break;
             case Jazz:
-                this->ctype = Note1;
+                ctype = Note1;
                 break;
             case Stereo:
-                this->ctype = Clef;
+                ctype = Clef;
                 duration = 0.5f;
                 break;
             case Star:
-                this->ctype = Star1;
+                ctype = Star1;
                 break;
             case Alien:
-                this->ctype = Star2;
+                ctype = Star2;
                 break;
             case Fantasia1:
-                this->ctype = Diamond;
+                ctype = Diamond;
                 break;
             case Fantasia2:
-                this->ctype = Unknown;
+                ctype = Unknown;
                 break;
         }
     }
 
-    switch(this->ctype) {
+    switch(ctype) {
         case Flower:
         case Note3:
-            this->anim_idle = new AnimationBoomerang(
+            anim_idle = new AnimationBoomerang(
                     string(level_ref->theme_base + "collectable" + type + "/"), 10, duration);
             break;
         default:
-            this->anim_idle = new AnimationLooping(
+            anim_idle = new AnimationLooping(
                     string(level_ref->theme_base + "collectable" + type + "/"), 10, duration);
             break;
     }
 
-    this->anim_collect = new Animation(level_ref->theme_base + "collect/", 10, 0.2f);
-    this->anim_collect->hide_on_complete = true;
+    anim_collect = new Animation(level_ref->theme_base + "collect/", 10, 0.2f);
+    anim_collect->hide_on_complete = true;
 
-    if (this->flying) {
-        this->r = 0xEE;
-        this->g = 0x42;
-        this->b = 0xF4;
+    if (flying) {
+        r = 0xEE;
+        g = 0x42;
+        b = 0xF4;
     } else {
-        this->r = this->g = 0xFF;
+        r = g = 0xFF;
     }
 }
 
 Collectable::~Collectable() {
-    delete this->anim_idle;
-    delete this->anim_collect;
+    delete anim_idle;
+    delete anim_collect;
 }
 
 void Collectable::update(float delta_time) {
     Entity::update(delta_time);
-    if (this->collected) {
-        this->anim_collect->advance(delta_time);
+    if (collected) {
+        anim_collect->advance(delta_time);
     } else {
-        this->anim_idle->advance(delta_time);
+        anim_idle->advance(delta_time);
     }
 }
 
 void Collectable::reset() {
     Entity::reset();
-    this->collected = false;
-    this->navigable_v = false;
-    this->anim_idle->reset();
-    this->anim_collect->reset();
+    collected = false;
+    navigable_v = false;
+    anim_idle->reset();
+    anim_collect->reset();
 }
 
 void Collectable::collect() {
-    if (!this->collected) {
+    if (!collected) {
         logger.info("Collected!");
         audio_manager.play_sfx(Collect);
-        this->collected = true;
-        this->navigable_v = true;
+        collected = true;
+        navigable_v = true;
     }
 }
 
 string Collectable::sprite() {
-    if (this->collected) {
-        return this->anim_collect->sprite();
+    if (collected) {
+        return anim_collect->sprite();
     }
-    return this->anim_idle->sprite();
+    return anim_idle->sprite();
 }
