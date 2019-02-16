13f7586d3ce7ea6acdf87e64225fb1167f231011
Tom Pickering <tom@alephspace.net>
2019-02-16 10:20:53 +0000

    Remove this->

--- src/anim/motion.cpp
+++ src/anim/motion.cpp
@@ -2,30 +2,30 @@
 
 #include "../shared.h"
 
-Motion::Motion(float x_target_offset, float y_target_offset, float duration) {
-    this->x_target_offset = x_target_offset;
-    this->y_target_offset = y_target_offset;
-    this->duration = duration;
-    this->reset();
+Motion::Motion(float _x_target_offset, float _y_target_offset, float _duration) {
+    x_target_offset = _x_target_offset;
+    y_target_offset = _y_target_offset;
+    duration = _duration;
+    reset();
 }
 
 Motion::~Motion() {
 }
 
 void Motion::reset() {
-    this->time = 0;
+    time = 0;
 }
 
 void Motion::advance(float delta) {
-    this->time += delta;
-    if (this->time > this->duration)
-        this->time = this->duration;
+    time += delta;
+    if (time > duration)
+        time = duration;
 }
 
 float Motion::get_x_offset() {
-    return x_target_offset * this->time / this->duration;
+    return x_target_offset * time / duration;
 }
 
 float Motion::get_y_offset() {
-    return y_target_offset * this->time / this->duration;
+    return y_target_offset * time / duration;
 }
