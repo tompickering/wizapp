10e4e7ba50bb4e01dbe3877cd9e91bba409ec5e5
Tom Pickering <tom@alephspace.net>
2023-06-06 19:52:52 +0100

    Cross-compilation framework

--- Makefile
+++ Makefile
@@ -1,29 +1,45 @@
 #!/usr/bin/make -f
 
+NAME=wizapp
+BIN=src/$(NAME)
+
 .PHONY: clean all install
 
 all:
 
 OBJS := $(patsubst %.cpp,%.o,$(wildcard *.cpp */*.cpp */*/*.cpp))
 DEPS = $(OBJS:%.o=%.d)
-CLEAN = $(NAME) $(OBJS) $(DEPS)
+CLEAN = $(NAME) $(BIN) $(OBJS) $(DEPS)
 
 LDLIBS=-lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf
-NAME=src/wizapp
-CXXFLAGS=-Wall -pedantic -std=c++14 -DSDL -DLINUX
+CXXFLAGS=-Wall -pedantic -std=c++14 -DSDL $(INCFLAGS)
 
 PREFIX = /usr/local
 
 %.d: %.cpp
 	$(CXX) -MM -MF $@ -MT $@ -MT $*.o $<
 
-all: $(NAME)
-	mv $(NAME) wizapp
+all: linux
 
-$(NAME):
+linux: ext=
+linux: CXXFLAGS += -DLINUX
+linux: bin
+
+# Requires mingw-64
+windows: CXX = x86_64-w64-mingw32-g++
+windows: ext=.exe
+windows: CXXFLAGS += -O3 -DWINDOWS
+windows: INCFLAGS += -Iinclude -Iinclude/x86_64-linux-gnu
+windows: LDLIBS := -Llib -static -l:libSDL2.dll.a -l:libSDL2_image.dll.a -l:libSDL2_mixer.dll.a -l:libSDL2_ttf.dll.a
+windows: bin
+
+bin: $(BIN)
+	mv $(BIN)$(ext) $(NAME)$(ext)
+
+$(BIN):
 	$(LINK.cpp) $^ $(LOADLIBES) $(LDLIBS) -o $@
 
-$(NAME): $(OBJS)
+$(BIN): $(OBJS)
 
 clean:
 	rm -rf $(CLEAN) core
---
+++ build_windows.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+git diff --exit-code >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "There are uncommitted changes; please resolve these before running"
+    exit
+fi
+
+mkdir -p include/x86_64-linux-gnu
+ln -sf /usr/include/SDL2 include
+ln -sf /usr/include/x86_64-linux-gnu/SDL2 include/x86_64-linux-gnu
+
+mkdir lib
+pushd lib
+wget -c https://github.com/libsdl-org/SDL/releases/download/release-2.26.5/SDL2-devel-2.26.5-mingw.tar.gz
+tar -xvf SDL2-devel-2.26.5-mingw.tar.gz
+cp SDL2-2.26.5/x86_64-w64-mingw32/lib/libSDL2.dll.a .
+wget -c https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.3/SDL2_image-devel-2.6.3-mingw.tar.gz
+tar -xvf SDL2_image-devel-2.6.3-mingw.tar.gz
+cp SDL2_image-2.6.3/x86_64-w64-mingw32/lib/libSDL2_image.dll.a .
+wget -c https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.6.3/SDL2_mixer-devel-2.6.3-mingw.tar.gz
+tar -xvf SDL2_mixer-devel-2.6.3-mingw.tar.gz
+cp SDL2_mixer-2.6.3/x86_64-w64-mingw32/lib/libSDL2_mixer.dll.a .
+wget -c https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.2/SDL2_ttf-devel-2.20.2-mingw.tar.gz
+tar -xvf SDL2_ttf-devel-2.20.2-mingw.tar.gz
+cp SDL2_ttf-2.20.2/x86_64-w64-mingw32/lib/libSDL2_ttf.dll.a .
+popd
+
+make clean
+make -j7 windows
+
+cp lib/SDL2-2.26.5/x86_64-w64-mingw32/bin/SDL2.dll .
+cp lib/SDL2_image-2.6.3/x86_64-w64-mingw32/bin/SDL2_image.dll .
+cp lib/SDL2_mixer-2.6.3/x86_64-w64-mingw32/bin/SDL2_mixer.dll .
+cp lib/SDL2_ttf-2.20.2/x86_64-w64-mingw32/bin/SDL2_ttf.dll .
+
+name=$(basename "$(pwd)")
+
+pushd ..
+zip -r wizapp.zip "${name}" --include "${name}/assets/*" "${name}/wizapp.exe" "${name}/LICENSE" "${name}/README.md" "${name}/*.dll"
+mv wizapp.zip "${name}"
+popd
